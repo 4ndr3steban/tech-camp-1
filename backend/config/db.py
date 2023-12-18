c4d7fc3a728c3869d4cbcbd3e8ce2ed83f09936e
4ndr3steban <amonsalvev@unal.edu.co>
2023-12-18 08:58:24 -0500

    fix db conection

--- backend/config/db.py
+++ backend/config/db.py
@@ -4,7 +4,7 @@ from .models import Base
 
 
 # Conexión a la base de datos MySql para datos de usuarios y queries
-engine = create_engine(f"mysql://{settings.MYSQL_USER}:{settings.MYSQL_PASS}@localhost:{settings.MYSQL_HOST}/{settings.MYSQL_DB}")
+engine = create_engine(f"mysql://{settings.MYSQL_USER}:{settings.MYSQL_PASS}@{settings.MYSQL_HOST}:3306/{settings.MYSQL_DB}")
 
 # Metadata para crear las tablas en la db y los tipos de datos en ellas
 Base.metadata.create_all(bind=engine)
